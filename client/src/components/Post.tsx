f9a8db33fa0ce2a231ba22260469e07eb639e70d
Evan Yang <71720197+Evany226@users.noreply.github.com>
2024-05-10 14:33:09 -0400

    more css finished

---
+++ client/src/components/Post.tsx
@@ -0,0 +1,10 @@
+const Post = () => {
+  return (
+    <div className="bg-white w-full p-4 flex-col justify-center items-center rounded-lg mt-4 border">
+      <div className="text-base text-gray-400">27 minutes ago</div>
+      <div className="text-lg mt-2 text-black">Hello how was your day</div>
+    </div>
+  );
+};
+
+export default Post;
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,7 +1,8 @@
 import Sidenav from "../components/Sidenav";
+import Post from "../components/Post";
 import { useState, useEffect, useRef } from "react";
 import {
-  TagIcon,
+  HashtagIcon,
   CodeBracketSquareIcon,
   PhotoIcon,
   DocumentCheckIcon,
@@ -31,12 +32,12 @@ const Dashboard = () => {
       <div className="w-full transition-all mx-auto flex flex-row justify-center items-center pl-60">
         <Sidenav />
         <main className="w-full h-auto flex flex-col items-center justify-center shrink bg-gray-100">
-          <section className="h-60 w-4/5 bg-slate-400 px-4 flex gap-4 p-6">
-            <div className="w-[calc(100%-15rem)]">
-              <div className="rounded-md mb-1 w-full flex flex-col justify-start items-start bg-white px-4 py-2 border border-gray-100 ">
+          <section className="w-full max-w-7xl bg-gray-100 px-4 flex gap-4 p-6 mb-0 pb-0">
+            <div className="w-[calc(100%-16rem)] h-full mb-8">
+              <div className="rounded-lg w-full h-full flex flex-col justify-start items-start px-4 py-2 border bg-white">
                 <div className="pb-2 flex flex-col justify-start items-start relative w-full h-auto max-h-[50vh] bg-inherit border-b ">
                   <textarea
-                    className="w-full h-full my-2 text-[0.95rem] resize-none overflow-x-hidden overflow-y-auto bg-transparent outline-none whitespace-pre-wrap word-break"
+                    className="w-full h-full my-2 ml-1 mt-5  text-lg resize-none overflow-x-hidden overflow-y-auto bg-transparent outline-none whitespace-pre-wrap word-break"
                     placeholder="Any thoughts..."
                     rows={1}
                     value={val}
@@ -44,29 +45,45 @@ const Dashboard = () => {
                     ref={textAreaRef}
                     style={{ color: "#000" }}
                   ></textarea>
-                  <div className="flex items-center mt-1">
-                    <TagIcon className="w-6 text-gray-600 ml-2" />
-                    <CodeBracketSquareIcon className="w-6 text-gray-600 ml-2" />
-                    <PhotoIcon className="w-6 text-gray-600 ml-2" />
-                    <DocumentCheckIcon className="w-6 text-gray-600 ml-2" />
+                  <div className="flex items-center my-2">
+                    <HashtagIcon className="w-7 text-gray-600 ml-1" />
+                    <CodeBracketSquareIcon className="w-7 text-gray-600 ml-2" />
+                    <PhotoIcon className="w-7 text-gray-600 ml-2" />
+                    <DocumentCheckIcon className="w-7 text-gray-600 ml-2" />
                   </div>
                 </div>
-                <div className="w-full pt-3 pb-1 flex justify-between items-center">
-                  <button className="flex justify-center items-center bg-gray-100 py-1 px-2 rounded-md border">
+                <div className="w-full py-4 flex justify-between items-center">
+                  <button className="flex justify-center items-center bg-gray-100 hover:bg-gray-300 py-1 px-2 rounded-md border">
                     <BellAlertIcon className="w-5 text-gray-700" />
-                    <p className="text-sm font-medium text-gray-700 ml-2">
+                    <p className="text-base font-medium text-gray-700 ml-2">
                       Reminders
                     </p>
-                    <ChevronDoubleDownIcon className="w-4 text-gray-700 ml-2 mt-0.5" />
+                    <ChevronDoubleDownIcon className="w-5 text-gray-700 ml-2 mt-0.5" />
                   </button>
-                  <button className="flex justify-center items-center bg-gray-200 py-1 px-2 rounded-md border">
-                    <p className="text-sm font-medium text-gray-700">Save</p>
-                    <PlusCircleIcon className="w-5 text-gray-700 ml-1" />
+                  <button className="flex justify-center items-center bg-gray-100 hover:bg-gray-300 py-1 px-2 rounded-md border">
+                    <p className="text-base font-medium text-gray-700">Save</p>
+                    <PlusCircleIcon className="w-6 text-gray-700 ml-1" />
                   </button>
                 </div>
               </div>
+
+              <Post />
+              <Post />
+              <Post />
+              <Post />
+              <Post />
+              <Post />
+              <Post />
+              <Post />
+              <Post />
+              <Post />
+              <Post />
+              <Post />
+              <Post />
             </div>
-            <div className="stick top-0 left-0 w-56 h-full bg-white"></div>
+            <div className="stick top-0 left-0 w-64 h-full bg-white">
+              <p className="h-48">Hello</p>
+            </div>
           </section>
         </main>
       </div>
