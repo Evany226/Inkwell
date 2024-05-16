07d284ff79feb00fbdb92be11ec7d2ea3fc67f17
Evan Yang <71720197+Evany226@users.noreply.github.com>
2024-05-16 18:33:57 -0400

    bug

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
-import Auth from "./pages/Auth";
+import Signup from "./pages/Signup";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
@@ -9,7 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/auth" element={<Auth />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,7 +1,15 @@
 const Signup = () => {
   return (
-    <div>hello </div>
-  )
+    <section className="rounded-md absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[30rem] h-96">
+      <h1>Hello</h1>
+      <div className="flex-col w-full h-full bg-white rounded-lg shadow p-2">
+        <h2 className="text-2xl font-bold p-3">Create an account</h2>
+      </div>
+      <form className="space-y-4">
+        <p>Hello</p>
+      </form>
+    </section>
+  );
 };
 
 export default Signup;
