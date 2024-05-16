48c88fd540dfa876f9ee00f4fdaa30d381dc508f
Evan Yang <71720197+Evany226@users.noreply.github.com>
2024-05-16 19:27:32 -0400

    signup login pages done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,12 +1,12 @@
 import GoogleButton from "../components/post/buttons/GoogleButton";
 import GithubButton from "../components/post/buttons/GithubButton";
 
-const Signup = () => {
+const Login = () => {
   return (
     <section className="flex flex-col items-center rounded-md absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[30rem] ">
       <img src="../../full-logo.png" className="w-52"></img>
       <div className="flex flex-col w-full h-full bg-white rounded-lg shadow p-8 mt-8">
-        <h2 className="text-2xl font-bold">Create an account</h2>
+        <h2 className="text-2xl font-bold">Sign in to your account</h2>
         <form className="mt-4 space-y-4 ">
           <div className="flex flex-col space-y-3">
             <label className="text-sm font-medium">Email Address</label>
@@ -24,27 +24,19 @@ const Signup = () => {
               placeholder="••••••••"
             ></input>
           </div>
-          <div className="flex flex-col space-y-3">
-            <label className="text-sm font-medium">Confirm password</label>
-            <input
-              type="confirm-password"
-              className="border border-gray-400 px-2 py-2 rounded-md text-sm focus:outline-violet-600 "
-              placeholder="••••••••"
-            ></input>
-          </div>
         </form>
         <div className="pt-8 pb-4">
           <button
             type="submit"
             className="w-full bg-violet-600 text-white py-1.5 text-sm font-medium rounded-md hover:bg-violet-800"
           >
-            Sign up
+            Log in
           </button>
         </div>
         <div className="flex justify-center pb-6">
           <p className="text-black text-sm font-medium">
-            Already have an account?{" "}
-            <a className="ml-1 text-violet-700 font-semibold">Log in</a>
+            Don't have an account?{" "}
+            <a className="ml-1 text-violet-700 font-semibold">Sign up</a>
           </p>
         </div>
         <div className="fter:h-px flex items-center before:h-px before:flex-1  before:bg-gray-300 before:content-[''] after:h-px after:flex-1 after:bg-gray-300  after:content-['']">
@@ -59,4 +51,4 @@ const Signup = () => {
   );
 };
 
-export default Signup;
+export default Login;
